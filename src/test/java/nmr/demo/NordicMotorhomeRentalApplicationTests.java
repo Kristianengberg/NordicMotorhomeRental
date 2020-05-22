58627135fe12d280aefed93abe3c46d12de2778f
Kristian Engberg <krissoengberg@gmail.com>
2020-05-22 13:21:55 +0200

    Initial commit

---
+++ src/test/java/nmr/demo/NordicMotorhomeRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package nmr.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NordicMotorhomeRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
