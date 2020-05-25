47f5965772a7b5c49a3a7ec344ea25caba4f144e
PR31SL3R <jesperpreisler@hotmail.com>
2020-05-25 11:45:53 +0200

    small fixes

--- src/main/java/nmr/demo/repositories/MotorhomeRepository.java
+++ src/main/java/nmr/demo/repositories/MotorhomeRepository.java
@@ -3,7 +3,13 @@ package nmr.demo.repositories;
 // import nmr.demo.util.DataBaseConnectionManager;
 
 import nmr.demo.models.MotorHome;
+import nmr.demo.utilities.DatabaseConnectionManager;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -11,13 +17,13 @@ import java.util.List;
 
 public class MotorhomeRepository implements IRepository<MotorHome> {
 
-    // private Connection conn;
+     private Connection conn;
 
 
-    /*public MotorhomeRepository() {
-        this.conn = DataBaseConnectionManager.getDataBaseConnection();
+    public MotorhomeRepository() {
+        this.conn = DatabaseConnectionManager.getDBConnection();
     }
-    */
+
 
 
     public boolean create(MotorHome model) {
@@ -31,8 +37,23 @@ public class MotorhomeRepository implements IRepository<MotorHome> {
 
     @Override
     public List<MotorHome> readAll() {
-        return null;
+        List<MotorHome> allAutoCampers = new ArrayList<>();
+        try {
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM autocamper");
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                MotorHome tempAutoCamper = new MotorHome();
+                tempAutoCamper.setBeds(rs.getInt(1));
+                tempAutoCamper.setModel(rs.getString(2));
+                allAutoCampers.add(tempAutoCamper);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return allAutoCampers;
     }
+
+
 
     @Override
     public boolean update(MotorHome model) {
