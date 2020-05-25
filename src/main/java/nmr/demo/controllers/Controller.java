f5a3214dd459ad14df3d937b04398e7fd5aa8877
PR31SL3R <jesperpreisler@hotmail.com>
2020-05-25 20:45:06 +0200

    small fixes

--- src/main/java/nmr/demo/controllers/Controller.java
+++ src/main/java/nmr/demo/controllers/Controller.java
@@ -1,20 +1,17 @@
 package nmr.demo.controllers;
 
 
-import nmr.demo.repositories.CustomerRepository;
+
 import nmr.demo.repositories.IRepository;
 import nmr.demo.repositories.MotorhomeRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.sql.SQLException;
-
 @Controller
 class AutocamperController {
 
     private IRepository autocamperRepository;
-    private IRepository customerRepository;
 
     public AutocamperController() {
         autocamperRepository = new MotorhomeRepository();
