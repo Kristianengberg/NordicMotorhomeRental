8cd25c113d5892171d06c0a06bcbcf63b5a39bae
ElmelundJC <Christianelmelund@hotmail.com>
2020-05-25 12:00:53 +0200

    Read and readAll functions

--- src/main/java/nmr/demo/repositories/MotorhomeRepository.java
+++ src/main/java/nmr/demo/repositories/MotorhomeRepository.java
@@ -7,7 +7,9 @@ import nmr.demo.models.MotorHome;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -30,12 +32,48 @@ public class MotorhomeRepository implements IRepository<MotorHome> {
 
     @Override
     public MotorHome read(int id) {
-        return null;
+        MotorHome MotorHomeToReturn = new MotorHome();
+        try {
+            PreparedStatement getSingleStudent = conn.prepareStatement("SELECT * FROM Motorhome WHERE id=" + id);
+            ResultSet rs = getSingleStudent.executeQuery();
+            while(rs.next()){
+                MotorHomeToReturn.setLicensePlateNo(rs.getString(1));
+                MotorHomeToReturn.setModel(rs.getString(2));
+                MotorHomeToReturn.setBeds(rs.getInt(3));
+                MotorHomeToReturn.setKilometers(rs.getDouble(4));
+                MotorHomeToReturn.setPrice(rs.getDouble(5));
+                MotorHomeToReturn.setEngineBlockNo(rs.getInt(6));
+                MotorHomeToReturn.setAccessible(rs.getString(7));
+
+            }
+        }
+        catch(SQLException s){
+            s.printStackTrace();
+        }
+        return MotorHomeToReturn;
     }
 
     @Override
     public List<MotorHome> readAll() {
-        return null;
+        List<MotorHome> allMotorHome = new ArrayList<MotorHome>();
+        try {
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM Motorhome");
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                MotorHome tempMotorHome = new MotorHome();
+                tempMotorHome.setLicensePlateNo(rs.getString(1));
+                tempMotorHome.setModel(rs.getString(2));
+                tempMotorHome.setBeds(rs.getInt(3));
+                tempMotorHome.setKilometers(rs.getDouble(4));
+                tempMotorHome.setPrice(rs.getDouble(5));
+                tempMotorHome.setEngineBlockNo(rs.getInt(6));
+                tempMotorHome.setAccessible(rs.getString(7));
+                allMotorHome.add(tempMotorHome);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return allMotorHome;
     }
 
     @Override
@@ -70,3 +108,4 @@ public class MotorhomeRepository implements IRepository<MotorHome> {
 
     }
 }
+
