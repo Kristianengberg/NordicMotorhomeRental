21fd58db6216919e6dcd33d6a5db3b7b78722592
JakobBundgaard <56150649+JakobBundgaard@users.noreply.github.com>
2020-05-28 09:35:41 +0200

    Create customer

--- src/main/java/nmr/demo/controllers/CustomerController.java
+++ src/main/java/nmr/demo/controllers/CustomerController.java
@@ -1,24 +1,48 @@
 package nmr.demo.controllers;
 
+import nmr.demo.models.Customer;
+import nmr.demo.models.MotorHome;
 import nmr.demo.repositories.AccessoryRepository;
 import nmr.demo.repositories.CustomerRepository;
 import nmr.demo.repositories.IRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CustomerController {
-    private IRepository customerRpository;
+    private IRepository customerRepository;
 
     public CustomerController() {
-        customerRpository = new CustomerRepository();
+        customerRepository = new CustomerRepository();
 
     }
 
     @GetMapping("/customer")
-    public String index(Model model) {
-        model.addAttribute("viewCustomers", customerRpository.readAll());
-        return "customer";
+    public String createCustomer(Model model){
+        model.addAttribute("newCustomer",new Customer());
+        return "/customer";
     }
+
+    @PostMapping("/saveCustomer")
+    public String saveCustomer(@ModelAttribute Customer customer){
+        customerRepository.create(customer);
+        return "/customerTemp";
+    }
+
+    @RequestMapping("/customerTemp")
+    public String getCustomerByParameterDetail(@RequestParam int id,Model model) {
+        model.addAttribute("customer",customerRepository.read(id));
+        return "/customerTemp";
+    }
+/*
+    @RequestMapping("/customerTemp/{id}")
+    public ModelAndView showAddProductPage(@PathVariable(name = "id") int id) {
+        ModelAndView mav = new ModelAndView("customerTemp");
+        Customer customer = (Customer) customerRepository.read(id);
+        mav.addObject("customer", customer);
+
+        return mav;
+    }*/
 }
