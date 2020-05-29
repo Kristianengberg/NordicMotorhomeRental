1ccfbabffbfd2153a319ebd64ef5e0f701dd8062
Kristian Engberg <krissoengberg@gmail.com>
2020-05-29 02:34:00 +0200

    dd

--- src/main/java/nmr/demo/controllers/InvoiceController.java
+++ src/main/java/nmr/demo/controllers/InvoiceController.java
@@ -6,28 +6,10 @@ import nmr.demo.models.MotorHome;
 import nmr.demo.repositories.IRepository;
 import nmr.demo.repositories.InvoiceRepository;
 import nmr.demo.repositories.MotorhomeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
-@Controller
-public class InvoiceController {
-
-    private IRepository invoiceRepository;
-
-
-import nmr.demo.models.Invoice;
-import nmr.demo.models.MotorHome;
-import nmr.demo.repositories.IRepository;
-import nmr.demo.repositories.InvoiceRepository;
-import nmr.demo.repositories.MotorhomeRepository;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class InvoiceController {
