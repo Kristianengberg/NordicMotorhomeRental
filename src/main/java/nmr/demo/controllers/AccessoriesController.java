a92e331dc6ea182141446f36e8bba9536e7234c6
JakobBundgaard <56150649+JakobBundgaard@users.noreply.github.com>
2020-05-30 10:58:11 +0200

    need accessories

--- src/main/java/nmr/demo/controllers/AccessoriesController.java
+++ src/main/java/nmr/demo/controllers/AccessoriesController.java
@@ -5,6 +5,7 @@ import nmr.demo.models.Customer;
 import nmr.demo.repositories.AccessoryRepository;
 import nmr.demo.repositories.IRepository;
 import nmr.demo.repositories.MotorhomeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +13,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+
 @Controller
 public class AccessoriesController {
     private IRepository accessoriesRepository;
 
+
+    @Autowired
     public AccessoriesController() {
         accessoriesRepository = new AccessoryRepository();
 
